165569b4759a2ef38f2f0be3041e9cc0df7b4fe6
Jeremy Geros <jeremy453@gmail.com>
2023-02-21 14:55:59 -0600

    groupings'

--- app/controllers/groupings_controller.rb
+++ app/controllers/groupings_controller.rb
@@ -2,6 +2,8 @@ class GroupingsController < ApplicationController
   before_action :require_user
 
   def show
+    get_dates
+
     if params[:category].present?
       @spends = Spend.where(category: params[:category])
       @name = params[:category].humanize
@@ -11,10 +13,22 @@ class GroupingsController < ApplicationController
     elsif params[:name].present?
       @spends = Spend.where(name: params[:name])
       @name = params[:name].humanize
+    elsif params[:recurring].present?
+      @name = 'Recurring transactions'
+      @transactions = Transaction.where(kind: :recurring)
+    elsif params[:one_off].present?
+      @name = 'One-off transactions'
+      @transactions = Transaction.where(kind: :one_off)
+    elsif params[:ignored].present?
+      @name = 'Ignored transactions'
+      @transactions = Transaction.where(ignored: true)
+    elsif params[:personal_transfer].present?
+      @name = 'Personal transfers'
+      @transactions = Transaction.where(personal_transfer: true)
     end
 
-    get_dates
-    @transactions = Transaction.where(spend: @spends).order(made_at: :desc, id: :desc).includes(spend: {icon_attachment: :blob}).where(made_at: @start_date..@end_date)
+    @transactions ||= Transaction.where(spend: @spends)
+    @transactions = @transactions.order(made_at: :desc, id: :desc).includes(:account, spend: {icon_attachment: :blob}).where(made_at: @start_date..@end_date)
   end
 
 end
